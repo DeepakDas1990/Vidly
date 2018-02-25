5ca10f2c917ce627ac6d7f883c4bcfe341d78717
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-02-25 16:27:59 +0530

    Exercise: Client Side Validation in New Movie Form

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -10,26 +10,27 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please Enter Name")]
         [StringLength(255)]
         public string Name { get; set; }
 
         public Genre Genre { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please Enter Genre")]
         [Display(Name ="Genre")]
         public byte GenreId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please Enter Release Date")]
         [Display(Name = "Release Date")]
         public DateTime ReleasedDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please Enter Added Date")]
         [Display(Name = "Added Date")]
         public DateTime AddedDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please Enter Number In Stock")]
         [Display(Name = "Number In Stock")]
+        [Range(1,20)]
         public Byte NumberInStock { get; set; }
     }
 }
