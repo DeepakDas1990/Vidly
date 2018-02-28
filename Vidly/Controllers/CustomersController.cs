8a31c1d027d90be0c6ff4b84557baf75427e0eda
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-01 00:05:06 +0530

    populating customer table with the help of data table and ajax from customer api

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -75,8 +75,7 @@ namespace Vidly.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = _Context.Customers.Include(c => c.MembershipType).ToList();
-            return View(customers);
+            return View();
         }
 
         // GET: Customers/Details/1
