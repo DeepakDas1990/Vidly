750060c6004bb790f9d76d85da8f1635757fd49f
Deepak Bidyabhusan Das <das.bidyabhusan@gmail.com>
2018-03-06 21:28:16 +0530

    Difensive Approach(Used in Public APIs)

--- Vidly/Controllers/API/NewRentalsController.cs
+++ Vidly/Controllers/API/NewRentalsController.cs
@@ -23,14 +23,24 @@ namespace Vidly.Controllers.API
 
         public IHttpActionResult CreateNewRentals(NewRentalDTO newRentalDTO)
         {
-            if (!ModelState.IsValid)
-                return BadRequest();
-
-            var movies = _context.Movies.Where(m => newRentalDTO.MovieIds.Contains(m.Id));
             var Customer = _context.Customers.Single(m => m.Id == newRentalDTO.CustomerId);
+            if (Customer == null)
+                return BadRequest("Customer Is Not Valid");
+
+            if (newRentalDTO.MovieIds.Count == 0)
+                return BadRequest("No Movie Is Selected");
+
+
+            var movies = _context.Movies.Where(m => newRentalDTO.MovieIds.Contains(m.Id)).ToList();
+
+            if (movies.Count != newRentalDTO.MovieIds.Count)
+                return BadRequest("One or More Movies Are not Valid");
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest(movie.Name + "Is Not Available");
+
                 movie.NumberAvailable--;
 
                 var NewRental = new Rental
